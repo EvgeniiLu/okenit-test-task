f81f6a408a2346b333df5a8d95bd79588b027b5a
Lushchai Evgenii <lushayevgen@mail.ru>
2022-04-19 21:14:26 +0300

    add load users and userposts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
-  <el-select v-model="value" clearable placeholder="Select">
+  <el-select
+    v-model="selectedName"
+    @change="loadUserPosts"
+    placeholder="Select User"
+  >
     <el-option
-      v-for="item in options"
-      :key="item.value"
+      v-for="item in users"
+      :key="item.name"
       :label="item.label"
-      :value="item.value"
+      :value="item.name"
     />
   </el-select>
-  <el-table :data="tableData" stripe style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
+  <el-table :data="posts" stripe style="width: 100%">
+    <el-table-column prop="title" label="Title" width="180" />
+    <el-table-column prop="body" label="Post" width="180" />
   </el-table>
 </template>
 
@@ -18,15 +21,46 @@
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 import { ref } from "vue";
+import { loadUsers } from "./api";
+import { loadPosts } from "./api";
+
 export default {
   name: "HomeView",
   components: {},
+
   data() {
     return {
-      value: ref(""),
-      options: [],
-      tableData: [],
+      selectedName: ref(""),
+      users: [],
+      posts: [],
     };
+  },
+
+  created: async function () {
+    const userList = await loadUsers();
+    userList.forEach((element) => {
+      this.users.push({
+        id: element.id,
+        name: element.name,
+        label: element.name,
+      });
+    });
+  },
+
+  methods: {
+    loadUserPosts() {
+      this.users.forEach(async (element) => {
+        if (this.selectedName === element.name) {
+          const userPosts = await loadPosts(`userId=${element.id}`);
+          userPosts.forEach((post) => {
+            this.posts.push({
+              title: post.title,
+              body: post.body,
+            });
+          });
+        }
+      });
+    },
   },
 };
 </script>
