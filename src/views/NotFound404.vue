43e33577bc10cb7a2dd8e8dc8f5a8b01f3ad32e4
Lushchai Evgenii <lushayevgen@mail.ru>
2022-04-23 22:57:29 +0300

    add notfound style

--- src/views/NotFound404.vue
+++ src/views/NotFound404.vue
@@ -1,1 +1,19 @@
-<template><div>Пустая страница</div></template>
+<template>
+  <div class="empty">
+    <div>Пустая страница</div>
+  </div>
+</template>
+<style scoped>
+.empty {
+  width: 100%;
+  height: 100vh;
+  background: black;
+  opacity: 0.8;
+  font-weight: 900;
+  font-size: 40px;
+  color: rgb(255, 255, 255);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
